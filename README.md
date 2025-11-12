c3bbe73e1feead70b78fd6dc8d11c6d280eeeb23
fawwazlabib29@gmail.com <fawwazz-z>
2025-11-12 08:33:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pas_mobile_11pplg2_16
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/pas_mobile_11pplg2_16/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pas_mobile_11pplg2_16
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
